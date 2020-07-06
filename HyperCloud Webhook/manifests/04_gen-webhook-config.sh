923741633b92f94cd8e0bfcef98fa1c563c61a26
gyeongyeol-choi <gyeongyeol_choi@tmax.co.kr>
2020-07-06 10:33:49 +0900

    Kuberntes 버전에 따른 webhook 배포 버전 설정

--- HyperCloud Webhook/manifests/04_gen-webhook-config.sh
+++ HyperCloud Webhook/manifests/04_gen-webhook-config.sh
@@ -6,5 +6,16 @@ if [ -f "$WEBHOOK_CONFIG_FILE" ]; then
    echo "Remove existed webhook config file."
    rm $WEBHOOK_CONFIG_FILE
 fi
+
+if [ $ADM_VERSION -eq "v1" ];then
+   ADM_VERSION2="["v1", "v1beta1"]"
+elif [ $ADM_VERSION -eq "v1beta1" ];then
+   ADM_VERSION2="["v1beta1"]"
+else
+   echo "ADM_VERSION is not defined!! Default Version is v1"
+   ADM_VERSION="v1"
+   ADM_VERSION2="["v1", "v1beta1"]"
+fi
+
 echo "Generate webhook config file."
 envsubst < ./"$WEBHOOK_CONFIG_FILE".template  > "$WEBHOOK_CONFIG_FILE"

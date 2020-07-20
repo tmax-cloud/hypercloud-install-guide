731e813182e316363869538af610c3fe9ef118e3
taeyoon_kim <taeyoon_kim@tmax.co.kr>
2020-07-20 05:51:17 +0000

    Modify script file

--- NetworkAgent/Network Webhook/patch.sh
+++ NetworkAgent/Network Webhook/patch.sh
@@ -133,6 +133,6 @@ else
     kubectl -n ${namespace} apply -f -
 fi
 
-# patch webhook.yaml
+# patch network-hook.yaml
 ca=$(kubectl config view --raw --minify --flatten -o jsonpath='{.clusters[].cluster.certificate-authority-data}')
-sed -i "/caBundle/c\    caBundle: $ca" webhook.yaml
+sed -i "/caBundle/c\    caBundle: $ca" network-hook.yaml

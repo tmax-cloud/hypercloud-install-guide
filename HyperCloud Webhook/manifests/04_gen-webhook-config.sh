f61bf895c758bb81601bdc90cb54ca9a51ca7a7f
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2020-08-04 21:24:44 +0900

    Update 04_gen-webhook-config.sh

--- HyperCloud Webhook/manifests/04_gen-webhook-config.sh
+++ HyperCloud Webhook/manifests/04_gen-webhook-config.sh
@@ -6,15 +6,6 @@ if [ -f "$WEBHOOK_CONFIG_FILE" ]; then
    echo "Remove existed webhook config file."
    rm $WEBHOOK_CONFIG_FILE
 fi
-if [ $ADM_VERSION = "v1" ];then
-   export ADM_VERSION2="[\"v1\", \"v1beta1\"]"
-elif [ $ADM_VERSION = "v1beta1" ];then
-   export ADM_VERSION2="[\"v1beta1\"]"
-else
-   echo "ADM_VERSION is not defined!! Default Version is v1"
-   export ADM_VERSION="v1"
-   export ADM_VERSION2="[\"v1\", \"v1beta1\"]"
-fi
 
 echo "Generate webhook config file."
 envsubst < ./"$WEBHOOK_CONFIG_FILE".template  > "$WEBHOOK_CONFIG_FILE"

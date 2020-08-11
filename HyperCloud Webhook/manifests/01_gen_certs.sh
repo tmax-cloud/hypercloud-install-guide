b83d8616e9b623922bfe92e02ecee7eb700bf3d2
chosangwon93 <66065293+chosangwon93@users.noreply.github.com>
2020-08-11 14:19:04 +0900

    Update 01_gen_certs.sh

--- HyperCloud Webhook/manifests/01_gen_certs.sh
+++ HyperCloud Webhook/manifests/01_gen_certs.sh
@@ -3,5 +3,3 @@ openssl req -nodes -new -x509 -keyout ./pki/ca.key -out ./pki/ca.crt -subj "/CN=
 openssl genrsa -out ./pki/hypercloud4-webhook.key 2048 && \
 openssl req -new -key ./pki/hypercloud4-webhook.key -subj "/CN=hypercloud4-webhook-svc.hypercloud4-system.svc" \
     | openssl x509 -req -CA ./pki/ca.crt -CAkey ./pki/ca.key -CAcreateserial -out ./pki/hypercloud4-webhook.crt -days 365
-#openssl pkcs12 -export -in ./pki/hypercloud4-webhook.crt -inkey ./pki/hypercloud4-webhook.key -out ./pki/hypercloud4-webhook.p12
-#keytool -importkeystore -deststorepass tmax@23 -destkeypass tmax@23 -destkeystore ./pki/hypercloud4-webhook.jks -srckeystore ./pki/hypercloud4-webhook.p12 -srcstoretype PKCS12 -srcstorepass tmax@23

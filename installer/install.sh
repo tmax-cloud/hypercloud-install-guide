4e9798c5474e1da164386462c497b64949867419
seonghoon_jeong <66111041+seonghoon-jeong@users.noreply.github.com>
2020-08-18 10:42:28 +0900

    Update install.sh

--- installer/install.sh
+++ installer/install.sh
@@ -35,7 +35,7 @@ function set_env() {
   	sudo modprobe overlay
     sudo modprobe br_netfilter
 
-    sudo cat << "EOF" | sudo tee -a /etc/sysctl.d/99-kubernetes-cri.conf
+    sudo cat << "EOF" | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf
     net.bridge.bridge-nf-call-iptables  = 1
     net.ipv4.ip_forward                 = 1
     net.bridge.bridge-nf-call-ip6tables = 1
